f98dd40ce4f99848ce93398f1afdad11bf4e9526
anks2020 <46972543+anks2020@users.noreply.github.com>
2019-05-18 09:26:36 +0530

    Add files via upload

---
+++ src/components/cards/MasonryList.js
@@ -0,0 +1,114 @@
+import '../../assets/css/custom.css';
+import React from 'react';
+import Masonry from 'react-masonry-component';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+// import IconButton from '@material-ui/core/IconButton';
+// import InfoIcon from '@material-ui/icons/Info';
+// import Modal from '../Modal/Modal';
+const masonryOptions = {
+    transitionDuration: 0,
+    column:3,
+    padding:4
+};
+const style={    margin:16};
+class Gallery extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            showdesc:true       }
+        // this.handleLayoutComplete= this.handleLayoutComplete.bind(this);
+        this.changeDateFormat= this.changeDateFormat.bind(this);
+        this.infoClicked= this.infoClicked.bind(this);
+    }
+    // handleLayoutComplete = (imagesLoadedInstance)=> {
+        // this.show();
+        // console.log(imagesLoadedInstance);
+    // }
+    infoClicked = ()=>{
+        this.setState({showdesc:!this.state.showdesc})
+    }
+    changeDateFormat =(d)=>{
+        var date = new Date(d);
+        var dt = date.toDateString();
+        var month;
+        var arr=dt.split(" ");
+        var day=arr[2];
+        var year = arr[3];
+        switch(arr[1])
+        {
+            case "Jan":
+                month="January"
+                break;
+            case "Feb":
+                month="February"
+                break;
+            case "Mar":
+                month="March"
+                break;
+            case "Apr":
+                month="April"
+                break;
+            case "May":
+                month="May"
+                break;
+            case "Jun":
+                month="June"
+                break;
+            case "Jul":
+                month="July"
+                break;
+            case "Aug":
+                month="August"
+                break;
+            case "Sep":
+                month="September"
+                break;
+            case "Oct":
+                month="October"
+                break;
+            case "Nov":
+                month="November"
+                break;
+            case "Dec":
+                month="December"
+                break;
+            default:
+                month="April"
+                break;
+        }
+        return (month+" "+day+" "+year)
+    }
+    render() {
+        // var show=this.state.showdesc;
+        // this.changeDateFormat(1313324536);
+        return (
+            <Masonry
+                className={'my-masonry-grid'}
+                style={style}
+                elementType={'ul'} // default 'div'
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+            >
+                {this.props.elements.map(function(element){
+           return (
+                <li style={{listStyleType: "none"}} key ={element.id} 
+                className="image-element-class"
+                >
+                    <img src={element.url} alt={element.title} width={300}/>
+                    <GridListTileBar 
+                    style={{width: 300,
+                        left: 5,
+                        bottom: 5
+                    }}
+                        title={element.title}
+                        subtitle={<span>by: {element.owner_name}</span>}
+                    />
+                </li>
+            )})}
+            </Masonry>
+        );
+    }
+}
+ 
+export default Gallery;
