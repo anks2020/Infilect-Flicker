dde49c90f63cc06df148deccefe9554e393c81fd
anks2020 <46972543+anks2020@users.noreply.github.com>
2019-05-18 10:52:20 +0530

    Update Gallery.js

--- src/containers/Screens/Gallery.js
+++ src/containers/Screens/Gallery.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-// import  '../../assets/css/custom.css';
 import Header from '../../components/Header/Header1';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/actions';
-import Grid from '../../components/cards/grid';
+import Masonry from '../../components/cards/MasonryList';
 class Gallery extends Component{
     constructor(props)
     {
@@ -22,21 +21,8 @@ class Gallery extends Component{
             <div>
                 <Header />
                 <div className="Bigdiv">
-                <Grid photos={this.props.photoList} />
-                {/* {this.props.photoList.map((photo)=>{
-                    let data=(<div className="ListI" key={photo.id}> */}
-                    {/* <Person style={{float:"right", cursor:"pointer"}} /><span className="userdata" > */}
-                    {/* <span style={{fontWeight:"bold", fontSize:"16px"}}>Title: </span><span>{photo.title}</span><br />
-                    <span style={{fontWeight:"bold", fontSize:"16px"}}>Owner: </span><span className="userdata" >{photo.owner_name}</span><br />
-                    <img src={photo.url} alt={photo.title} width={200} height={200} crossOrigin="anonymous" />
-                    <span style={{fontWeight:"bold", fontSize:"16px"}}>Posted_at: </span><span className="userdata" >{photo.posted_at}</span><br />
-                    <span style={{fontWeight:"bold", fontSize:"16px"}}>Description: </span><span className="userdata" >{photo.description}</span><br /> */}
-                    {/* </div>)
-                    return data;
-                })
-                } */}
-            {/* }        */}
-               
+                <h1>{this.props.location.state.group_name}</h1>
+                <Masonry elements={this.props.photoList} />
                 </div>
             </div>)
         }
